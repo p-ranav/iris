6e9bd591bc777d423aa3f1023fbaabc43a5d1456
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-04-01 15:51:37 -0700

    Updated timers example

--- samples/interval_timers/interval_timers.cpp
+++ samples/interval_timers/interval_timers.cpp
@@ -4,7 +4,10 @@
 
 int main() {
   iris::component timers;
-  timers.set_interval(500, []() { std::cout << "Ping\n"; });
-  timers.set_interval(1000, []() { std::cout << "Pong\n"; });
+  timers.set_interval(250, []() { std::cout << "Timer_1\n"; });
+  timers.set_interval(500, []() { std::cout << "Timer_2\n"; });
+  timers.set_interval(1000, []() { std::cout << "Timer_3\n"; });
+  timers.set_interval(2000, []() { std::cout << "Timer_4\n"; });
+  timers.set_interval(5000, []() { std::cout << "Timer_5\n"; });
   timers.start();
 }
