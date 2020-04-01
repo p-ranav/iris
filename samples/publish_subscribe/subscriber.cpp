af0071264e02f7f2801bbd098e71e3e865489a24
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-04-01 15:18:55 -0700

    Minor update

--- samples/publish_subscribe/subscriber.cpp
+++ samples/publish_subscribe/subscriber.cpp
@@ -3,7 +3,6 @@
 #include <iris/subscriber.hpp>
 
 int main() {
-
   iris::component receiver;
   receiver.create_subscriber({"tcp://localhost:5555"}, [](std::string msg) {
     std::cout << "Received " << msg << "\n";
