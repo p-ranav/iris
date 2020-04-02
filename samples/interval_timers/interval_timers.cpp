1f3b196dc91dbba173f4390e2cdcf9970f0ae7f0
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-04-02 10:34:27 -0700

    Minor update

--- samples/interval_timers/interval_timers.cpp
+++ samples/interval_timers/interval_timers.cpp
@@ -4,10 +4,10 @@
 
 int main() {
   iris::component timers;
-  timers.set_interval(250, []() { std::cout << "Timer_1\n"; });
-  timers.set_interval(500, []() { std::cout << "Timer_2\n"; });
-  timers.set_interval(1000, []() { std::cout << "Timer_3\n"; });
-  timers.set_interval(2000, []() { std::cout << "Timer_4\n"; });
-  timers.set_interval(5000, []() { std::cout << "Timer_5\n"; });
+  timers.set_interval(period =  250, []() { std::cout << "Timer_1\n"; });
+  timers.set_interval(period =  500, []() { std::cout << "Timer_2\n"; });
+  timers.set_interval(period = 1000, []() { std::cout << "Timer_3\n"; });
+  timers.set_interval(period = 2000, []() { std::cout << "Timer_4\n"; });
+  timers.set_interval(period = 5000, []() { std::cout << "Timer_5\n"; });
   timers.start();
 }
