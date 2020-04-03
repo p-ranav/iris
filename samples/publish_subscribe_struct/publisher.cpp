70b1239a74f9f6692ef636c2c76070cb460339e1
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-04-03 09:41:30 -0700

    Added struct serialization sample

---
+++ samples/publish_subscribe_struct/publisher.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <random>
+#include <iris/iris.hpp>
+using namespace iris;
+
+struct Mouse {
+  int x, y;
+
+  // Method for serialization
+  template <typename Archive>
+  void save(Archive& ar) const {
+    ar(x, y);
+  }
+};
+
+int main() {
+  std::random_device rd; // obtain a random number from hardware
+  std::mt19937 eng(rd()); // seed the generator
+  std::uniform_int_distribution<> x_dist(0, 1920);
+  std::uniform_int_distribution<> y_dist(0, 1080);
+
+  Component sender(threads = 1);
+  auto p = sender.create_publisher(endpoints = {"tcp://*:5555"});
+
+  sender.set_interval(period = 250, 
+    on_expiry = [&] { 
+        Mouse position {.x = x_dist(eng), .y = y_dist(eng)};
+
+        p.send(position);
+        std::cout << "Published (" << position.x << ", " << position.y << ")\n";
+  });
+  sender.start();
+}
---
+++ samples/publish_subscribe_struct/subscriber.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iris/iris.hpp>
+using namespace iris;
+
+struct Mouse {
+  int x, y;
+
+  // Method for deserialization
+  template <typename Archive>
+  void load(Archive& ar) {
+    ar(x, y);
+  }
+};
+
+int main() {
+  Component receiver(threads = 1);
+  receiver.create_subscriber(endpoints = {"tcp://localhost:5555"},
+    on_receive = [](Message msg) {
+      auto position = msg.get<Mouse>();
+      std::cout << "Received ("
+        << position.x << ", " << position.y << ")\n";
+  });
+  receiver.start();
+}
