ebe17dc28654244ac06f0c061f49edeb485dad7c
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-04-04 00:15:26 -0500

    Tested fast client-server

--- samples/client_server_struct/client.cpp
+++ samples/client_server_struct/client.cpp
@@ -14,7 +14,7 @@ int main() {
   auto client =
       c.create_client(endpoints = {"tcp://127.0.0.1:5510"}, timeout = 2500);
   c.set_interval(
-      period = 200, on_expiry = [&] {
+      period = 50, on_expiry = [&] {
         std::cout << "Sending Ping\n";
         auto response = client.send("Ping");
         std::cout << "Received status code: "
