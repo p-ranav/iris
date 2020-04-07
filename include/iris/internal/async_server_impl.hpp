be1a576ad6d694fa0e2e42a489accb3f4ee81026
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-04-06 21:00:33 -0500

    Removed unnecessary change

--- include/iris/internal/async_server_impl.hpp
+++ include/iris/internal/async_server_impl.hpp
@@ -62,14 +62,6 @@ public:
   void recv();
 
   template <typename Response> void send(Response &&response) {
-
-    // If response not set by user, set to empty JSON object
-    if (!response.set_) {
-      std::string serialized = "{\"set\": 0}"; // empty JSON
-      response.payload_.rebuild(serialized.size());
-      memcpy(response.payload_.data(), serialized.c_str(), serialized.size());
-    }
-
     auto success = socket_->send(response.payload_, zmq::send_flags::none);
     while (!success) {
       socket_->send(response.payload_, zmq::send_flags::none);
